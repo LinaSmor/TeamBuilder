434ec9e50cddbd8c8ab2bac2c9bab3f1e463a91c
alex zotkin <alex.z806@mail.ru>
2021-05-19 16:45:48 +0300

    исправления и новый функционал к защите

---
+++ Controllers/HowitworksController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamBuilder.Models;
+
+namespace TeamBuilder.Controllers
+{
+    public class HowitworksController : Controller
+    {
+        private readonly DataBaseContext db;
+
+        public HowitworksController(DataBaseContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewData["ProfileVisible"] = HttpContext.Request.Cookies.ContainsKey("UserData");
+            if ((bool)ViewData["ProfileVisible"])
+            {
+                int VkId = int.Parse(HttpContext.Request.Cookies["UserData"]);
+                User User = await db.Users.Where(u => u.VkId == VkId).FirstAsync();
+                ViewData["FirstName"] = User.FirstName;
+                ViewData["LastName"] = User.LastName;
+                ViewData["Photo50"] = User.Photo50;
+            }
+            return View();
+        }
+    }
+}
